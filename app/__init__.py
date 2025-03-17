f5e14ece1d1dd94e2083634211eaa0131a5d19b0
zhaoyan21 <1727919865@qq.com>
2025-03-18 02:19:01 +0800

    更新

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from app.Model import db
 from config import Config
+from flask_migrate import Migrate
 
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    migrate.init_app(app, db)
     with app.app_context():
         db.create_all()
     return app
--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+import os
+
+
 class Config:
     SECRET_KEY = 'ZHAOYAN'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///lab.db'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance/lab.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_SECURE = True  # 仅HTTPS传输Cookie
